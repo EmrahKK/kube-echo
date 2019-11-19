584a14a1bf7a02922e85fbaffc5d32de4a1845b3
emrah.kucukkaya <emrah.kucukkaya@ibb.gov.tr>
2019-11-19 11:32:48 +0300

    add gitlab-ci.yaml

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const port = 3000
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/json', (req, res) => res.json({"message":"Hello World"}))
 
+app.get('/echo', (req, res) => {
+    let name = req.query.name;
+    let message = "Hollo "+name;
+    res.json({"message": message})
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
