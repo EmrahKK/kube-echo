b94932a2ad5c0fc983a1eed76cf8d19e98b9bf2f
emrah.kucukkaya <emrah.kucukkaya@ibb.gov.tr>
2019-11-28 11:32:46 +0300

    add healtz endpoint

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ app.get('/echo', (req, res) => {
     res.json({"message": message})
 })
 
+app.get('/healthz', (req, res) => {
+  var date = new Date();
+  var current_hour = date.getHours();
+  var current_minutes = date.getMinutes();
+  if (current_hour==11 && current_minutes<31 && current_minutes>30) {    
+    res.status(400).json({"Message": "Unhealty.."})  
+  } else {
+    res.json({"Message": "Healty.."})  
+  }
+  
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
