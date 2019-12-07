eec4847cac4ffb7a5faa652e7906da6902fdaffc
emrah.kucukkaya <emrah.kucukkaya@ibb.gov.tr>
2019-12-07 15:56:47 +0300

    log method added

--- index.js
+++ index.js
@@ -5,28 +5,40 @@ const port = 3000
 const startDate = Date.now();
 
 app.get('/', (req, res) => {
-  const seconds = Math.floor(Date.now() - startDate)/1000;
-  res.json({"Uptime(seconds)": seconds})    
+  const seconds = Math.floor(Date.now() - startDate) / 1000;
+  res.json({"Uptime(seconds)": seconds})
 })
 
-app.get('/json', (req, res) => res.json({"message":"Hello World"}))
+app.get('/json', (req, res) => res.json({"message": "Hello World"}))
 app.get('/echo', (req, res) => {
-    let name = req.query.name;
-    let message = "Hollo "+name;
-    res.json({"message": message})
+  let name = req.query.name;
+  let message = "Hollo " + name;
+  res.json({"message": message})
 })
 
 // http status codes
-app.get('/badRequest', (req, res) => {    
-    res.status(400).json({"message": "Bad request"})
+app.get('/badRequest', (req, res) => {
+  res
+    .status(400)
+    .json({"message": "Bad request"})
 })
 
-app.get('/notFound', (req, res) => {    
-    res.status(404).json({"message": "Not found"})
+app.get('/notFound', (req, res) => {
+  res
+    .status(404)
+    .json({"message": "Not found"})
 })
 
-app.get('/internalServiceError', (req, res) => {    
-    res.status(500).json({"message": "Internal service error"})
+app.get('/internalServiceError', (req, res) => {
+  res
+    .status(500)
+    .json({"message": "Internal service error"})
+})
+
+app.post('/log', (req, res) => {
+  let log = "" + Date.now();
+  console.log(log)
+  res.send(log);
 })
 
 // Kubernetes
@@ -34,21 +46,25 @@ app.get('/healthz', (req, res) => {
   var date = new Date();
   var current_hour = date.getHours();
   var current_minutes = date.getMinutes();
-  
-  if (current_hour==13 && current_minutes<2 ) {    
-    res.status(400).json({"Message": "Unhealty..","current_hour": current_hour,"current_minutes": current_minutes})  
+
+  if (current_hour == 13 && current_minutes < 2) {
+    res
+      .status(400)
+      .json({"Message": "Unhealty..", "current_hour": current_hour, "current_minutes": current_minutes})
   } else {
-    res.json({"Message": "Healty..","current_hour": current_hour,"current_minutes": current_minutes})  
-  }  
+    res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes})
+  }
 })
 
 app.get('/readyz', (req, res) => {
-    const seconds = Math.floor(Date.now() - startDate)/1000;
-    if (seconds<20) {
-      res.status(400).json({"Message": "Not Ready.."})  
-    } else {
-      res.json({"Uptime(seconds)": seconds})  
-    };    
+  const seconds = Math.floor(Date.now() - startDate) / 1000;
+  if (seconds < 20) {
+    res
+      .status(400)
+      .json({"Message": "Not Ready.."})
+  } else {
+    res.json({"Uptime(seconds)": seconds})
+  };
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
