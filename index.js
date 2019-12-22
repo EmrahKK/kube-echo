27e29805280530aae6bb500f0e3fa0af6205d7ba
emrah.kucukkaya <emrah.kucukkaya@ibb.gov.tr>
2019-12-22 14:51:26 +0300

    test commit without tag

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ var bodyParser = require('body-parser');
 
 const app = express();
 const port = 80;
-
 app.use(bodyParser.text({
   type: function (req) {
     return 'text';
   }
 }));
-
 const startDate = Date.now();
-
 app.get('/', (req, res) => {
   const seconds = Math.floor(Date.now() - startDate) / 1000;
   res.json({"Uptime(seconds)": seconds});
@@ -31,19 +28,16 @@ app.get('/badRequest', (req, res) => {
     .status(400)
     .json({"message": "Bad request"});
 });
-
 app.get('/notFound', (req, res) => {
   res
     .status(404)
     .json({"message": "Not found"});
 });
-
 app.get('/internalServiceError', (req, res) => {
   res
     .status(500)
     .json({"message": "Internal service error"});
 });
-
 app.post('/log', (req, res) => {    
   res = res.status(200);
   if (req.get('Content-Type')) {    
@@ -52,7 +46,6 @@ app.post('/log', (req, res) => {
   console.log(req.body);
   res.send(req.body);    
 });
-
 // Kubernetes
 app.get('/healthz', (req, res) => {
   var date = new Date();
@@ -67,7 +60,6 @@ app.get('/healthz', (req, res) => {
     res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes});
   }
 });
-
 app.get('/readyz', (req, res) => {
   const seconds = Math.floor(Date.now() - startDate) / 1000;
   if (seconds < 20) {
@@ -78,5 +70,4 @@ app.get('/readyz', (req, res) => {
     res.json({"Uptime(seconds)": seconds});
   }
 });
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
