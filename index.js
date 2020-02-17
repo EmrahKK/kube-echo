b11edce94103fc17f5dc312ff23774062f1f5e23
emrah.kucukkaya <emrah.kucukkaya@ibb.gov.tr>
2020-02-17 16:12:07 +0300

    update index

--- index.js
+++ index.js
@@ -77,13 +77,8 @@ app.get('/healthz', (req, res) => {
   }
 });
 app.get('/readyz', (req, res) => {
-  const seconds = Math.floor(Date.now() - startDate) / 1000;
-  if (seconds < 20) {
-    res
-      .status(400)
-      .json({"Message": "Not Ready.."});
-  } else {
-    res.json({"Uptime(seconds)": seconds});
-  }
+  //const seconds = Math.floor(Date.now() - startDate) / 1000;
+  res.json({"Uptime(seconds)": seconds});
+  
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
