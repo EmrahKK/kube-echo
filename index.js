ba4e677c55192fae0f1cee278224658501a50afa
emrah.kucukkaya <emrah.kucukkaya@ibb.gov.tr>
2020-02-17 16:16:39 +0300

    update

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ app.get('/echo', (req, res) => {
   console.log(message);
   res.json({"message": message});
 });
-
 // http status codes
 app.get('/badRequest', (req, res) => {
   res
@@ -64,21 +63,23 @@ app.get('/sleep', (req, res) => {
 });
 // Kubernetes
 app.get('/healthz', (req, res) => {
-  var date = new Date();
-  var current_hour = date.getHours();
-  var current_minutes = date.getMinutes();
+  res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes});
+  // var date = new Date();
+  // var current_hour = date.getHours();
+  // var current_minutes = date.getMinutes();
 
-  if (current_hour == 13 && current_minutes < 2) {
-    res
-      .status(400)
-      .json({"Message": "Unhealty..", "current_hour": current_hour, "current_minutes": current_minutes});
-  } else {
-    res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes});
-  }
+  // if (current_hour == 13 && current_minutes < 2) {
+  //   res
+  //     .status(400)
+  //     .json({"Message": "Unhealty..", "current_hour": current_hour, "current_minutes": current_minutes});
+  // } else {
+  //   res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes});
+  // }
+  
 });
 app.get('/readyz', (req, res) => {
   //const seconds = Math.floor(Date.now() - startDate) / 1000;
   res.json({"Uptime(seconds)": seconds});
-  
+
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
