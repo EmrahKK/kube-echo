b9d616ca0f344bf1786719c521b9025ec12c918d
emrah.kucukkaya <emrah.kucukkaya@ibb.gov.tr>
2020-02-18 08:54:24 +0300

    18.02.2020 08:54

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/json', (req, res) => res.json({"message": "Hello World"}));
-
 app.get('/echo', (req, res) => {
   let name = req.query.name;
   let message = "Hollo " + name;
   console.log(message);
   res.json({"message": message});
 });
+
 // http status codes
 app.get('/badRequest', (req, res) => {
   res
@@ -64,22 +64,26 @@ app.get('/sleep', (req, res) => {
 });
 // Kubernetes
 app.get('/healthz', (req, res) => {
-  res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes});
-  // var date = new Date();
-  // var current_hour = date.getHours();
-  // var current_minutes = date.getMinutes();
+  var date = new Date();
+  var current_hour = date.getHours();
+  var current_minutes = date.getMinutes();
 
-  // if (current_hour == 13 && current_minutes < 2) {
-  //   res
-  //     .status(400)
-  //     .json({"Message": "Unhealty..", "current_hour": current_hour, "current_minutes": current_minutes});
-  // } else {
-  //   res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes});
-  // }  
+  if (current_hour == 13 && current_minutes < 1) {
+    res
+      .status(400)
+      .json({"Message": "Unhealty..", "current_hour": current_hour, "current_minutes": current_minutes});
+  } else {
+    res.json({"Message": "Healty..", "current_hour": current_hour, "current_minutes": current_minutes});
+  }
 });
 app.get('/readyz', (req, res) => {
-  //const seconds = Math.floor(Date.now() - startDate) / 1000;
-  res.json({"Uptime(seconds)": seconds});
-
+  const seconds = Math.floor(Date.now() - startDate) / 1000;
+  if (seconds < 15) {
+    res
+      .status(400)
+      .json({"Message": "Not Ready.."});
+  } else {
+    res.json({"Uptime(seconds)": seconds});
+  }
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
